0a4b0fd9fed09cc02a6f7cfd66db348c90f8ce4b
Kirbseverywhere <35226583+Kirbseverywhere@users.noreply.github.com>
2019-10-05 21:04:52 +0200

    Add Music
    And fix more glitches
    And change caduceus' effect

--- Engine Hacks/MapSpritePalettes/src/MapSpritesPalette.c
+++ Engine Hacks/MapSpritePalettes/src/MapSpritesPalette.c
@@ -13,26 +13,30 @@ unsigned GetUnitMapSpritePaletteIndex(struct Unit *unit) {
 	}
 	else if(gChapterData.unitColorOption == 0) {
 		// Check allegiance
-		int allegiance = unit->index >> 6;
+		int allegiance = (u8)(unit->index) >> 6;
 		switch(allegiance) {
 			case 0: // Player
 				return 0xB;
-			case 1: // Enemy
-				return 0xE;
-			case 2: // Neutral
+			case 1: // Neutral
 				return 0xD;
+			case 2: // Enemy
+				return 0xE;
+			case 3: // Enemy
+				return 0xE;
 		}
 	}
 	else {
 		// Check allegiance
-		int allegiance = unit->index >> 6;
+		int allegiance = (u8)(unit->index) >> 6;
 		switch(allegiance) {
 			case 0: // Player
 				return 0xB;
 			case 1: // Enemy
-				return 0xC;
-			case 2: // Neutral
 				return 0xD;
+			case 2: // Neutral
+				return 0xC;
+			case 3: // Neutral
+				return 0xC;
 		}
 	}
 }
