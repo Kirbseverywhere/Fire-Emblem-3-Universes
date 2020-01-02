a98ea98a2883b40794a76dad9c11606dcf8a49e0
Sme <esahig15789@gmail.com>
2020-01-02 16:18:14 -0500

    Growth getter fixes
    Mag growth getter still borked

--- Engine Hacks/ExternalHacks/StrMagSplit/GrowthGetters/Get_Luk_Growth.asm
+++ Engine Hacks/ExternalHacks/StrMagSplit/GrowthGetters/Get_Luk_Growth.asm
@@ -5,6 +5,10 @@
 ldr		r1,[r0]
 add		r1,#34
 ldrb	r1,[r1]		@luk growth
+ldr 	r2, [r0, #4]
+add		r2, #0x21
+ldrb	r2, [r2]
+add 	r1, r2
 mov		r2,#16		@index of luk boost
 ldr		r3,Extra_Growth_Boosts
 bx		r3
